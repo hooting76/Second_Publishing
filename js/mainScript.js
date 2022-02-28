79614150cf9465912364a49f404b7c3ec84c8786
hooting76 <60126117+hooting76@users.noreply.github.com>
2022-02-28 18:17:07 +0900

    반응형페이지 뎁스영역 작업중

---
+++ js/mainScript.js
@@ -0,0 +1,8 @@
+// $('.lang').hover(function(){
+//     $('.lang>a:nth-child(2)').animate().css({top:40});
+//     console.log($('.lang>a:nth-child(2)'));
+//     $('a[tabindex=8]').animate().css({top:80});
+// },function(){
+//     $('a[tabindex=7]').animate().css({top:0});
+//     $('a[tabindex=8]').animate().css({top:00});
+// });
