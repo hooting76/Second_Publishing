ff17bf2acff362efa4c1d0fc43bbffb81dead387
hooting76 <60126117+hooting76@users.noreply.github.com>
2022-03-04 18:17:20 +0900

    뎁스영역작업완료/배너&퀵메뉴 작업중

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,8 +1,24 @@
-// $('.lang').hover(function(){
-//     $('.lang>a:nth-child(2)').animate().css({top:40});
-//     console.log($('.lang>a:nth-child(2)'));
-//     $('a[tabindex=8]').animate().css({top:80});
-// },function(){
-//     $('a[tabindex=7]').animate().css({top:0});
-//     $('a[tabindex=8]').animate().css({top:00});
-// });
+setInterval(videoResizeFn,100);
+$(window).resize(function(){
+    videoResizeFn();
+});
+
+function videoResizeFn(){
+    winH=$(window).innerHeight();
+    winW=$(window).innerWidth();
+    imgH=$('.banner_inner ul li').innerHeight();
+    // console.log(imgH);
+    imgW=$('.banner_inner ul li').innerWidth();
+    // console.log(imgW);
+
+    $('.banner_inner ul').css({width:100+"%", height:winH});
+    if(winH>imgH){// 창의 높이보다 이미지높이가 크다면
+        $('.banner_inner ul li').css({width:"auto", height:winH});
+    }
+    if(winW>imgW){// 창너비보다 이미지너비가 작으면
+        $('.banner_inner ul li').css({width:winW, height:"auto"});
+    }
+
+    $('.banner_inner ul li').css({marginTop:(winH-imgH)/2, marginLeft: (winW-imgW)/2});
+    // 추후 수정필요 3/4
+}
