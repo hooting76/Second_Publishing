bfc1d248d8ae73a95040f364fb3e5f5a8f2f37e6
hooting76 <60126117+hooting76@users.noreply.github.com>
2022-03-08 18:17:45 +0900

    메뉴선택시 강조효과 추가+js수정중

--- js/mainScript.js
+++ js/mainScript.js
@@ -9,16 +9,16 @@ function videoResizeFn(){
     imgH=$('.banner_inner ul li').innerHeight();
     // console.log(imgH);
     imgW=$('.banner_inner ul li').innerWidth();
-    // console.log(imgW);
+    console.log(imgW);  //<-값은 잘 들어옴
 
     $('.banner_inner ul').css({width:100+"%", height:winH});
-    if(winH>imgH){// 창의 높이보다 이미지높이가 크다면
-        $('.banner_inner ul li').css({width:"auto", height:winH});
-    }
+    // if(winH>imgH){// 창의 높이보다 이미지높이가 크다면
+    //     $('.banner_inner ul li').css({width:"auto", height:winH});
+    // }
     if(winW>imgW){// 창너비보다 이미지너비가 작으면
-        $('.banner_inner ul li').css({width:winW, height:"auto"});
+        $('.banner_inner ul li img').css({width:winW});
     }
 
     $('.banner_inner ul li').css({marginTop:(winH-imgH)/2, marginLeft: (winW-imgW)/2});
-    // 추후 수정필요 3/4
+    // 추후 수정필요 3/8
 }
